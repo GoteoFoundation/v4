f162e7e3814fc74cfead3554c728ded51f7135d3
Daniel Subiabre <daniel.subiabre@platoniq.net>
2024-02-21 15:18:25 +0100

    Update gateway state processor to decorate persits processor

--- src/State/GatewayCheckoutStateProcessor.php
+++ src/State/GatewayCheckoutStateProcessor.php
@@ -6,26 +6,23 @@ use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use App\Entity\GatewayCheckout;
 use App\Library\Economy\Payment\GatewayLocator;
-use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
 
+#[AsDecorator('api_platform.doctrine.orm.state.persist_processor')]
 class GatewayCheckoutStateProcessor implements ProcessorInterface
 {
     public function __construct(
+        private ProcessorInterface $innerProcessor,
         private GatewayLocator $gatewayLocator,
-        private EntityManagerInterface $entityManager,
     ) {
     }
 
-    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): GatewayCheckout
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
     {
-        /** @var GatewayCheckout */
-        $gatewayCheckout = $data;
+        if ($data instanceof GatewayCheckout) {
+            $gateway = $this->gatewayLocator->getGatewayOf($data);
+        }
 
-        $gateway = $this->gatewayLocator->getGatewayOf($gatewayCheckout);
-
-        $this->entityManager->persist($gatewayCheckout);
-        $this->entityManager->flush($gatewayCheckout);
-
-        return $gatewayCheckout;
+        return $this->innerProcessor->process($data, $operation, $uriVariables, $context);
     }
 }
