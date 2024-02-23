6e3494e8413658a7a352b29ada80080f6d6d46e3
Daniel Subiabre <daniel.subiabre@platoniq.net>
2024-02-23 20:01:07 +0100

    Add Transactions

--- src/Entity/Accounting.php
+++ src/Entity/Accounting.php
@@ -4,10 +4,12 @@ namespace App\Entity;
 
 use ApiPlatform\Metadata as API;
 use App\Repository\AccountingRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * Accountings are the senders and receivers of Transactions.
+ * Accountings are the receivers and the issuers of Transactions.
  */
 #[ORM\Entity(repositoryClass: AccountingRepository::class)]
 #[API\ApiResource]
@@ -19,8 +21,80 @@ class Accounting
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\OneToMany(mappedBy: 'target', targetEntity: Transaction::class)]
+    private Collection $transactionsReceived;
+
+    #[ORM\OneToMany(mappedBy: 'origin', targetEntity: Transaction::class)]
+    private Collection $transactionsIssued;
+
+    public function __construct()
+    {
+        $this->transactionsReceived = new ArrayCollection();
+        $this->transactionsIssued = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return Collection<int, Transaction>
+     */
+    public function getTransactionsReceived(): Collection
+    {
+        return $this->transactionsReceived;
+    }
+
+    public function addTransactionsReceived(Transaction $transactionsReceived): static
+    {
+        if (!$this->transactionsReceived->contains($transactionsReceived)) {
+            $this->transactionsReceived->add($transactionsReceived);
+            $transactionsReceived->setTarget($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTransactionsReceived(Transaction $transactionsReceived): static
+    {
+        if ($this->transactionsReceived->removeElement($transactionsReceived)) {
+            // set the owning side to null (unless already changed)
+            if ($transactionsReceived->getTarget() === $this) {
+                $transactionsReceived->setTarget(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Transaction>
+     */
+    public function getTransactionsIssued(): Collection
+    {
+        return $this->transactionsIssued;
+    }
+
+    public function addTransactionsIssued(Transaction $transactionsIssued): static
+    {
+        if (!$this->transactionsIssued->contains($transactionsIssued)) {
+            $this->transactionsIssued->add($transactionsIssued);
+            $transactionsIssued->setOrigin($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTransactionsIssued(Transaction $transactionsIssued): static
+    {
+        if ($this->transactionsIssued->removeElement($transactionsIssued)) {
+            // set the owning side to null (unless already changed)
+            if ($transactionsIssued->getOrigin() === $this) {
+                $transactionsIssued->setOrigin(null);
+            }
+        }
+
+        return $this;
     }
 }
---
+++ src/Entity/Transaction.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\TransactionRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Transactions represent a movement of money from one Accounting (origin) into another (target).\
+ * \
+ * When a Transaction targets an Accounting it means that the Accounting receives it, this will add to that Accounting.
+ * When a Transaction originates from an Accounting the Accounting issues the Transaction and it will deduct from it.
+ */
+#[ORM\Entity(repositoryClass: TransactionRepository::class)]
+#[ApiResource]
+class Transaction
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    /**
+     * The monetary value to be received at target and issued at origin.
+     */
+    #[Assert\Valid()]
+    #[Assert\NotBlank()]
+    #[ORM\Embedded(class: Money::class)]
+    private ?Money $money = null;
+
+    /**
+     * The Accounting receiving the money of this Transaction.
+     */
+    #[Assert\NotBlank()]
+    #[ORM\ManyToOne(inversedBy: 'transactionsReceived')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Accounting $target = null;
+
+    /**
+     * The Accounting which issued the money of this Transaction.
+     */
+    #[ORM\ManyToOne(inversedBy: 'transactionsIssued')]
+    private ?Accounting $origin = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMoney(): ?Money
+    {
+        return $this->money;
+    }
+
+    public function setMoney(Money $money): static
+    {
+        $this->money = $money;
+
+        return $this;
+    }
+
+    public function getTarget(): ?Accounting
+    {
+        return $this->target;
+    }
+
+    public function setTarget(?Accounting $target): static
+    {
+        $this->target = $target;
+
+        return $this;
+    }
+
+    public function getOrigin(): ?Accounting
+    {
+        return $this->origin;
+    }
+
+    public function setOrigin(?Accounting $origin): static
+    {
+        $this->origin = $origin;
+
+        return $this;
+    }
+}
