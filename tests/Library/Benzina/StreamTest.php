2d12ab2405a4f40da2d5e02610e2fd36a5cc15fb
Daniel Subiabre <daniel.subiabre@platoniq.net>
2024-03-18 17:03:22 +0100

    Add tests for benzina stream

---
+++ tests/Library/Benzina/StreamTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Tests\Library\Benzina;
+
+use App\Library\Benzina\Stream;
+use PHPUnit\Framework\TestCase;
+
+class StreamTest extends TestCase
+{
+    private const CHUNK_SIZE = 100;
+    private const STREAM_SIZE = 1000;
+
+    private Stream $stream;
+
+    public function setUp(): void
+    {
+        $this->stream = new Stream(\str_repeat("0", self::STREAM_SIZE));
+    }
+
+    public function testStreamReadsLength()
+    {
+        $chunk = $this->stream->read(self::CHUNK_SIZE + 1);
+
+        $this->assertEquals(\str_repeat("0", self::CHUNK_SIZE), $chunk);
+    }
+
+    public function testStreamTells()
+    {
+        $this->assertEquals(0, $this->stream->tell());
+
+        $chunk = $this->stream->read(self::CHUNK_SIZE);
+
+        $this->assertEquals(\strlen($chunk), $this->stream->tell());
+    }
+}
