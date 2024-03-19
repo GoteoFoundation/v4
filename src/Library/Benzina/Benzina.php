c29b9805f031859b34564653c84860b61175f407
Daniel Subiabre <daniel.subiabre@platoniq.net>
2024-03-19 14:18:20 +0100

    Burn it up

---
+++ src/Library/Benzina/Benzina.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Library\Benzina;
+
+use App\Library\Benzina\Pump\PumpInterface;
+use App\Library\Benzina\Stream\StreamInterface;
+
+class Benzina
+{
+    /** @var Pump\PumpInterface[] */
+    private array $availablePumps = [];
+
+    public function __construct(
+        iterable $instanceof
+    ) {
+        $this->availablePumps = \iterator_to_array($instanceof);
+    }
+
+    /**
+     * Get the Pumps that can process a sample in the stream data.
+     * 
+     * @var mixed $sample
+     * @return PumpInterface[]
+     */
+    public function getPumps(StreamInterface $stream, int $sampleSize = 1): array
+    {
+        $sample = $stream->read($sampleSize);
+        $stream->rewind();
+
+        $pumps = [];
+        foreach ($this->availablePumps as $pump) {
+            if (!$pump->supports($sample)) {
+                continue;
+            }
+
+            $pumps[] = $pump;
+        }
+
+        return $pumps;
+    }
+}
