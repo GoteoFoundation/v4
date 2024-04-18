c71fbdf8efe890b886e0cc06a5ab06b0efead3e2
Daniel Subiabre <daniel.subiabre@platoniq.net>
2024-04-18 14:33:08 +0200

    Allow to filter users by email, name or username

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,9 +2,9 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Metadata as API;
 use App\Entity\Interface\UserOwnedInterface;
+use App\Filter\UserQueryFilter;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -29,10 +29,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 #[API\Put(security: 'is_granted("USER_EDIT", object)')]
 #[API\Delete(security: 'is_granted("USER_EDIT", object)')]
 #[API\Patch(security: 'is_granted("USER_EDIT", object)')]
-#[API\ApiFilter(filterClass: SearchFilter::class, properties: [
-    'username' => 'partial',
-    'name' => 'partial'
-])]
+#[API\ApiFilter(filterClass: UserQueryFilter::class, properties: ['query'])]
 #[UniqueEntity(fields: ['username'], message: 'This usernames already exists.')]
 #[UniqueEntity(fields: ['email'], message: 'This email address is already registered.')]
 #[ORM\Entity(repositoryClass: UserRepository::class)]
