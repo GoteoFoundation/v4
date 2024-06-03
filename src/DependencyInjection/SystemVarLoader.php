50d6297e7d1bb9185f64268ff868075aa595d86d
Daniel Subiabre <daniel.subiabre@platoniq.net>
2024-06-03 13:43:34 +0200

    Catch db not setup exceptions in SystemVarLoader to allow app to continue

--- src/DependencyInjection/SystemVarLoader.php
+++ src/DependencyInjection/SystemVarLoader.php
@@ -3,6 +3,8 @@
 namespace App\DependencyInjection;
 
 use App\Repository\SystemVarRepository;
+use Doctrine\DBAL\Exception\ConnectionException;
+use Doctrine\DBAL\Exception\TableNotFoundException;
 use Symfony\Component\DependencyInjection\EnvVarLoaderInterface;
 
 /**
@@ -17,7 +19,15 @@ final class SystemVarLoader implements EnvVarLoaderInterface
 
     public function loadEnvVars(): array
     {
-        $systemVars = $this->systemVarRepository->findAll();
+        try {
+            $systemVars = $this->systemVarRepository->findAll();
+        } catch (ConnectionException $e) {
+            // Ignore loader in environments where the database is not yet setup
+            return [];
+        } catch (TableNotFoundException $e) {
+            // Ignore loader in environments where the table does not exist
+            return [];
+        }
 
         $processedVars = [];
         foreach ($systemVars as $systemVar) {
