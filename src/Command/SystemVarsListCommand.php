66d5a290c1176127719c6e9ac54c425c91c0ea92
Daniel Subiabre <daniel.subiabre@platoniq.net>
2024-06-03 13:22:37 +0200

    Allow to list stored system vars

---
+++ src/Command/SystemVarsListCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\SystemVar;
+use App\Repository\SystemVarRepository;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:system:vars:list'
+)]
+class SystemVarsListCommand extends Command
+{
+    public function __construct(
+        private SystemVarRepository $systemVarRepository
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $io->table(['Name', 'Value'], \array_map(function (SystemVar $systemVar) {
+            return [$systemVar->getName(), $systemVar->getValue()];
+        }, $this->systemVarRepository->findAll()));
+
+        return Command::SUCCESS;
+    }
+}
