5ba62e8a018d8db778b3b86ade7580f991b3d99b
David Igón <david.igon@riseup.net>
2024-07-01 11:25:22 +0200

    fix using php cs fixer

--- tests/Library/Economy/Currency/EuropeanCentralBankExchangeTest.php
+++ tests/Library/Economy/Currency/EuropeanCentralBankExchangeTest.php
@@ -9,7 +9,7 @@ class EuropeanCentralBankExchangeTest extends TestCase
 {
     public function testGetsData()
     {
-        $exchange = new EuropeanCentralBankExchange;
+        $exchange = new EuropeanCentralBankExchange();
         $exchangeData = $exchange->getData();
 
         $this->assertIsArray($exchangeData);
@@ -19,7 +19,7 @@ class EuropeanCentralBankExchangeTest extends TestCase
 
     public function testStoresDataInApcuCache()
     {
-        $exchange = new EuropeanCentralBankExchange;
+        $exchange = new EuropeanCentralBankExchange();
         // Data should be available on instantiation
 
         $data = \apcu_fetch($exchange::ECB_DATA);
@@ -27,7 +27,7 @@ class EuropeanCentralBankExchangeTest extends TestCase
         $this->assertNotFalse($data);
 
         $exchangeData = \json_decode($data, true);
-        
+
         $this->assertIsArray($exchangeData);
         $this->assertArrayHasKey('Cube', $exchangeData);
         $this->assertArrayHasKey('@attributes', $exchangeData);
