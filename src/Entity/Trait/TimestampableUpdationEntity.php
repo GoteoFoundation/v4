f00249af04e798dfffc93493b1e7941d496905b2
Daniel Subiabre <daniel.subiabre@platoniq.net>
2024-07-25 11:49:16 +0200

    Make custom timestampable entity traits

---
+++ src/Entity/Trait/TimestampableUpdationEntity.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entity\Trait;
+
+use ApiPlatform\Metadata as API;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+trait TimestampableUpdationEntity
+{
+    /**
+     * Entity last update timestamp.
+     *
+     * @var \DateTime|null
+     */
+    #[API\ApiProperty(writable: false)]
+    #[Gedmo\Timestampable(on: 'update')]
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    protected $updatedAt;
+
+    /**
+     * Sets updatedAt.
+     *
+     * @return $this
+     */
+    public function setUpdatedAt(\DateTime $updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Returns updatedAt.
+     *
+     * @return \DateTime|null
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+}
