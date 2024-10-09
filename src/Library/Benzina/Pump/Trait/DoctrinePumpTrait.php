b7ad6ed7bd3060061c7d1220924574aceb95dd6c
Daniel Subiabre <daniel.subiabre@platoniq.net>
2024-10-09 15:43:05 +0200

    Update DoctrinePumpTrait

--- src/Library/Benzina/Pump/Trait/DoctrinePumpTrait.php
+++ src/Library/Benzina/Pump/Trait/DoctrinePumpTrait.php
@@ -9,7 +9,11 @@ trait DoctrinePumpTrait
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
-    ) {}
+    ) {
+        if (!$this instanceof PumpInterface) {
+            throw new \Exception("DoctrinePumpTrait can only be used with PumpInterface classes");
+        }
+    }
 
     /**
      * Skip already pumped records from a pumping stream batch.
@@ -26,19 +30,15 @@ trait DoctrinePumpTrait
         string $entityClass,
         string $entityKey,
     ): array {
-        if (!$this instanceof PumpInterface) {
-            throw new \Exception("DoctrinePumpTrait can only be used with PumpInterface classes");
-        }
-
         if (!$this->getConfig('skip-pumped')) {
             return $batch;
         }
 
-        $repository = $this->entityManager->getRepository($entityClass);
-
-        $pumped = $repository->findBy([$entityKey => \array_map(function ($record) use ($batchKey) {
-            return $record[$batchKey];
-        }, $batch)]);
+        $pumped = $this->entityManager
+            ->getRepository($entityClass)
+            ->findBy([$entityKey => \array_map(function (array $record) use ($batchKey) {
+                return $record[$batchKey];
+            }, $batch)]);
 
         $pumpedKeys = [];
         foreach ($pumped as $key => $entity) {
