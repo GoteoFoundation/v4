d2ab2d98fe1f62f75020fdf386d286539d14a3ab
Daniel Subiabre <daniel.subiabre@platoniq.net>
2024-10-28 15:09:44 +0100

    Avoid double persisting entity

--- src/EventListener/AccountingOwnerListener.php
+++ src/EventListener/AccountingOwnerListener.php
@@ -35,9 +35,6 @@ final class AccountingOwnerListener
             $accounting = new Accounting();
             $accounting->setOwner($entity);
 
-            $this->entityManager->persist($accounting);
-            $this->entityManager->flush();
-
             $entity->setAccounting($accounting);
 
             $this->entityManager->persist($entity);
