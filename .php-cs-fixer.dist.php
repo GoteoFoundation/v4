621b0b96d825919fa6480f71529e0502e771e5cf
Daniel Subiabre <daniel.subiabre@platoniq.net>
2024-11-07 15:15:26 +0100

    No space after fn for short arrow functions

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -8,6 +8,7 @@ return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
         'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline'],
+        'function_declaration' => ['closure_fn_spacing' => 'none'],
         'single_line_empty_body' => true,
         'yoda_style' => [
             'equal' => false,
