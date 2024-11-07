5d24dfaac3a6341caecd828651714a6327b4e539
Daniel Subiabre <daniel.subiabre@platoniq.net>
2024-11-07 11:41:40 +0100

    Allow to map charge to entity

--- src/Mapping/Gateway/ChargeMapper.php
+++ src/Mapping/Gateway/ChargeMapper.php
@@ -5,11 +5,13 @@ namespace App\Mapping\Gateway;
 use App\ApiResource\Gateway as Resource;
 use App\Entity\Gateway as Entity;
 use App\Mapping\Accounting\AccountingMapper;
+use App\Repository\Gateway\ChargeRepository;
 
 class ChargeMapper
 {
     public function __construct(
         private AccountingMapper $accountingMapper,
+        private ChargeRepository $chargeRepository,
     ) {}
 
     public function toResource(Entity\Charge $entity): Resource\Charge
@@ -23,5 +25,22 @@ class ChargeMapper
         $resource->target = $this->accountingMapper->toResource($entity->getTarget());
 
         return $resource;
+    }
+
+    public function toEntity(Resource\Charge $resource): Entity\Charge
+    {
+        $entity = new Entity\Charge();
+
+        if ($resource->id !== null) {
+            $entity = $this->chargeRepository->find($resource->id);
+        }
+
+        $entity->setType($resource->type);
+        $entity->setTitle($resource->title);
+        $entity->setDescription($resource->description);
+        $entity->setMoney($resource->money);
+        $entity->setTarget($this->accountingMapper->toEntity($resource->target));
+
+        return $entity;
     }
 }
