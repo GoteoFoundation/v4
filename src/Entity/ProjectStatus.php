d3e45197a09c390dafba5da11296595301da04ba
Daniel Subiabre <daniel.subiabre@platoniq.net>
2024-11-27 14:54:31 +0100

    Update project statuses

--- src/Entity/ProjectStatus.php
+++ src/Entity/ProjectStatus.php
@@ -2,13 +2,43 @@
 
 namespace App\Entity;
 
+/**
+ * Projects have a start and an end, and in the meantime they go through different phases represented under this status.
+ */
 enum ProjectStatus: string
 {
+    /**
+     * Project is under edition by it's owner.
+     */
+    case InEditing = 'in_editing';
+
+    /**
+     * Owner finished editing and an admin is reviewing it.
+     */
+    case InReview = 'in_review';
+
+    /**
+     * An admin reviewed it and rejected it. Final.
+     */
     case Rejected = 'rejected';
-    case Editing = 'editing';
-    case Reviewing = 'reviewing';
+
+    /**
+     * Project was reviewed and is in campaign for funding.
+     */
     case InCampaign = 'in_campaign';
-    case Funded = 'funded';
-    case Fulfilled = 'fulfilled';
+
+    /**
+     * Project finished campaigning but didn't meet funding goals. Final.
+     */
     case Unfunded = 'unfunded';
+
+    /**
+     * Project successfully finished campaigning and owner can receive funds.
+     */
+    case InFunding = 'in_funding';
+
+    /**
+     * Project owner received funds and fulfilled their goals. Final.
+     */
+    case Fulfilled = 'fulfilled';
 }
