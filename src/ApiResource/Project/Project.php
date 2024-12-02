71f8e9811778a5937e4c27869c6a9e3d676fc182
Daniel Subiabre <daniel.subiabre@platoniq.net>
2024-12-02 14:40:01 +0100

    Update project api resource

--- src/ApiResource/Project/Project.php
+++ src/ApiResource/Project/Project.php
@@ -2,12 +2,14 @@
 
 namespace App\ApiResource\Project;
 
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Doctrine\Orm\State\Options;
 use ApiPlatform\Metadata as API;
 use App\ApiResource\Accounting\Accounting;
 use App\Entity\Project as Entity;
 use App\Entity\Project\ProjectStatus;
 use App\Entity\User;
+use App\State\ApiResourceStateProcessor;
 use App\State\Project\ProjectStateProvider;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -16,8 +18,14 @@ use Symfony\Component\Validator\Constraints as Assert;
  */
 #[API\ApiResource(
     stateOptions: new Options(entityClass: Entity\Project::class),
-    provider: ProjectStateProvider::class
+    provider: ProjectStateProvider::class,
+    processor: ApiResourceStateProcessor::class
 )]
+#[API\GetCollection()]
+#[API\Post(security: 'is_granted("ROLE_USER")')]
+#[API\Get()]
+#[API\Patch(security: 'is_granted("PROJECT_EDIT")')]
+#[API\Delete(security: 'is_granted("PROJECT_EDIT")')]
 class Project
 {
     #[API\ApiProperty(identifier: true, writable: false)]
@@ -26,6 +34,7 @@ class Project
     /**
      * The User who launched this Project.
      */
+    #[API\ApiFilter(filterClass: SearchFilter::class, strategy: 'exact')]
     #[API\ApiProperty(writable: false)]
     public User $owner;
 
@@ -38,6 +47,7 @@ class Project
     /**
      * Main title for this Project.
      */
+    #[API\ApiFilter(filterClass: SearchFilter::class, strategy: 'partial')]
     #[Assert\NotBlank()]
     public string $title;
 
@@ -45,6 +55,10 @@ class Project
      * The current status of this Project.\
      * Admin-only property.
      */
+    #[API\ApiFilter(filterClass: SearchFilter::class, strategy: 'exact')]
     #[API\ApiProperty(securityPostDenormalize: 'is_granted("ROLE_ADMIN")')]
     public ProjectStatus $status = ProjectStatus::InEditing;
+
+    /** @var array<int, Reward> */
+    public array $rewards = [];
 }
