0950604465d58918d2efeeb9807f9508edeb9bab
Daniel Subiabre <daniel.subiabre@platoniq.net>
2024-12-02 14:40:15 +0100

    Add generic api resource state processor

---
+++ src/State/ApiResourceStateProcessor.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Doctrine\Common\State\PersistProcessor;
+use ApiPlatform\Doctrine\Common\State\RemoveProcessor;
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProcessorInterface;
+use AutoMapper\AutoMapperInterface;
+use ApiPlatform\Doctrine\Orm\State\Options;
+use ApiPlatform\Metadata\DeleteOperationInterface;
+use App\Entity\Interface\AccountingOwnerInterface;
+use App\Entity\Interface\UserOwnedInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
+class ApiResourceStateProcessor implements ProcessorInterface
+{
+    public function __construct(
+        #[Autowire(service: RemoveProcessor::class)]
+        private ProcessorInterface $removeProcessor,
+        #[Autowire(service: PersistProcessor::class)]
+        private ProcessorInterface $persistProcessor,
+        private AutoMapperInterface $autoMapper,
+        private Security $security,
+    ) {}
+
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
+    {
+        /** @var Options */
+        $stateOptions = $operation->getStateOptions();
+        $entityClass = $stateOptions->getEntityClass();
+
+        $target = new $entityClass();
+
+        /** @var object */
+        $entity = $this->autoMapper->map($data, $target, $this->buildMappingContext($data, $target));
+
+        if ($operation instanceof DeleteOperationInterface) {
+            $this->removeProcessor->process($entity, $operation, $uriVariables, $context);
+
+            return null;
+        }
+
+        if ($entity->getId() === null && $entity instanceof UserOwnedInterface) {
+            $entity->setOwner($this->security->getUser());
+        }
+
+        $this->persistProcessor->process($entity, $operation, $uriVariables, $context);
+        $data->id = $entity->getId();
+
+        return $data;
+    }
+
+    private function buildMappingContext(object $source, object $target): array
+    {
+        $ignoredAttributes = [];
+
+        if (!isset($source->id)) {
+            \array_push($ignoredAttributes, 'id');
+
+            if ($target instanceof UserOwnedInterface && !isset($source->owner)) {
+                \array_push($ignoredAttributes, 'owner');
+            }
+
+            if ($target instanceof AccountingOwnerInterface && !isset($source->accounting)) {
+                \array_push($ignoredAttributes, 'accounting');
+            }
+        }
+
+        return [
+            'ignored_attributes' => $ignoredAttributes
+        ];
+    }
+}
