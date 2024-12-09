3d1edf621b651f4f04b64ddd3ad80a14461e3843
Daniel Subiabre <daniel.subiabre@platoniq.net>
2024-12-09 09:08:33 +0100

    Decorated auto mapper service

---
+++ src/Mapping/AutoMapper.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Mapping;
+
+use AutoMapper\AutoMapper as InnerMapper;
+use AutoMapper\AutoMapperInterface;
+
+class AutoMapper implements AutoMapperInterface
+{
+    private AutoMapperInterface $innerMapper;
+
+    public function __construct(
+        iterable $mapProviders
+    ) {
+        $this->innerMapper = InnerMapper::create(
+            providers: $mapProviders
+        );
+    }
+
+    public function map(array|object $source, string|array|object $target, array $context = []): array|object|null
+    {
+        return $this->innerMapper->map($source, $target, $context);
+    }
+}
