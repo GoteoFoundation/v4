0b7d978fdfb315cc43eb7f4d66e17f3e6b516663
Daniel Subiabre <daniel.subiabre@platoniq.net>
2024-12-11 11:59:52 +0100

    Update Project state processor

--- src/State/Project/ProjectStateProcessor.php
+++ src/State/Project/ProjectStateProcessor.php
@@ -2,38 +2,55 @@
 
 namespace App\State\Project;
 
+use ApiPlatform\Doctrine\Common\State\PersistProcessor;
+use ApiPlatform\Doctrine\Common\State\RemoveProcessor;
+use ApiPlatform\Metadata\DeleteOperationInterface;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use App\ApiResource\Project\ProjectApiResource;
-use App\ApiResource\User\UserApiResource;
 use App\Entity\Project\Project;
 use App\Mapping\AutoMapper;
 use App\Repository\User\UserRepository;
-use App\State\ApiResourceStateProcessor;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class ProjectStateProcessor implements ProcessorInterface
 {
     public function __construct(
+        #[Autowire(service: RemoveProcessor::class)]
+        private ProcessorInterface $deleteProcessor,
+        #[Autowire(service: PersistProcessor::class)]
+        private ProcessorInterface $persistProcessor,
+        private AutoMapper $autoMapper,
         private Security $security,
         private UserRepository $userRepository,
-        private AutoMapper $autoMapper,
-        private ApiResourceStateProcessor $stateProcessor
     ) {}
 
     /**
      * @param ProjectApiResource $data
      * 
-     * @return Project|null
+     * @return ProjectApiResource|null
      */
-    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []) {
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
+    {
+        /** @var Project */
+        $project = $this->autoMapper->map($data, Project::class, ['skip_null_values' => true]);
+
         if (!isset($data->id)) {
             $user = $this->security->getUser();
-            $owner = $this->userRepository->findOneByIdentifier($user->getUserIdentifier());
+            $owner = $this->userRepository->findOneBy(['username' => $user->getUserIdentifier()]);
 
-            $data->owner = $this->autoMapper->map($owner, UserApiResource::class);
+            $project->setOwner($owner);
         }
 
-        return $this->stateProcessor->process($data, $operation, $uriVariables, $context);
+        if ($operation instanceof DeleteOperationInterface) {
+            $this->deleteProcessor->process($project, $operation, $uriVariables, $context);
+
+            return null;
+        }
+
+        $this->persistProcessor->process($project, $operation, $uriVariables, $context);
+
+        return $this->autoMapper->map($project, $data);
     }
 }
