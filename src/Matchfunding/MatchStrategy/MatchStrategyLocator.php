7a3ca00db5af59e5cf869f6285cb36862d3ca7b4
Daniel Subiabre <daniel.subiabre@platoniq.net>
2024-12-19 12:37:30 +0100

    Throw custom exceptions for match strategies

--- src/Matchfunding/MatchStrategy/MatchStrategyLocator.php
+++ src/Matchfunding/MatchStrategy/MatchStrategyLocator.php
@@ -3,27 +3,26 @@
 namespace App\Matchfunding\MatchStrategy;
 
 use App\Entity\Matchfunding\MatchCall;
+use App\Matchfunding\MatchStrategy\Exception\MatchStrategyDuplicatedException;
+use App\Matchfunding\MatchStrategy\Exception\MatchStrategyNotFoundException;
 
 class MatchStrategyLocator
 {
     /** @var array<string, MatchStrategyInterface> */
-    private array $strategiesByName;
+    private array $strategiesByName = [];
 
     public function __construct(
         iterable $strategies,
     ) {
-        $strategies = \iterator_to_array($strategies);
-
         foreach ($strategies as $strategy) {
             $strategyName = $strategy::getName();
 
             if (\array_key_exists($strategyName, $this->strategiesByName)) {
-                throw new \Exception(\sprintf(
-                    "Duplicate MatchStrategy name '%s' by '%s', value already in use by '%s'",
+                throw new MatchStrategyDuplicatedException(
                     $strategyName,
                     $strategy::class,
                     $this->strategiesByName[$strategyName]::class
-                ));
+                );
             }
 
             $this->strategiesByName[$strategyName] = $strategy;
@@ -33,10 +32,7 @@ class MatchStrategyLocator
     public function get(string $strategyName): ?MatchStrategyInterface
     {
         if (!array_key_exists($strategyName, $this->strategiesByName)) {
-            throw new \Exception(\sprintf(
-                "Could not find a MatchStrategy by the name '%s', value does not exist",
-                $strategyName
-            ));
+            throw new MatchStrategyNotFoundException($strategyName);
         }
 
         return $this->strategiesByName[$strategyName];
