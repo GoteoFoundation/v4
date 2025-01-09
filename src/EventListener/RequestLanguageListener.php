2e6ff5a3bb266a9c66823fa69fecaf09846e6112
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-01-09 14:56:23 +0100

    Retrieve locale from accept-language request header

--- src/EventListener/RequestLanguageListener.php
+++ src/EventListener/RequestLanguageListener.php
@@ -9,6 +9,11 @@ use Symfony\Component\HttpKernel\KernelEvents;
 #[AsEventListener(event: KernelEvents::REQUEST, method: 'processLanguage', priority: 101)]
 final class RequestLanguageListener
 {
+    /**
+     * Sets the request locale from language headers
+     * - `Content-Language` for write requests
+     * - `Accept-Language` for read requests
+     */
     public function processLanguage(RequestEvent $event): void
     {
         $request = $event->getRequest();
@@ -16,6 +21,11 @@ final class RequestLanguageListener
         $contentLanguage = $request->headers->get('Content-Language');
         if ($contentLanguage !== null) {
             $request->setLocale($contentLanguage);
+        }
+
+        $acceptLanguage = $request->headers->get('Accept-Language');
+        if ($acceptLanguage !== null) {
+            $request->setLocale($acceptLanguage);
         }
     }
 }
