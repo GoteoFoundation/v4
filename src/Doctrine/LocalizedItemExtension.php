7abaee0e975e9259f44017ab061d7295e9e32fc8
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-01-14 16:09:59 +0100

    Add localized item doctrine extension

---
+++ src/Doctrine/LocalizedItemExtension.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Doctrine;
+
+use ApiPlatform\Doctrine\Orm\Extension\QueryResultItemExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Metadata\Operation;
+use App\Entity\Interface\LocalizedContentInterface;
+use Doctrine\ORM\QueryBuilder;
+
+/**
+ * Adds localization hints to translatable entity queries.
+ *
+ * @see https://github.com/doctrine-extensions/DoctrineExtensions/blob/main/doc/translatable.md#using-orm-query-hint
+ */
+final class LocalizedItemExtension implements QueryResultItemExtensionInterface
+{
+    use LocalizedContentTrait;
+
+    /**
+     * Same priority as `api_platform.doctrine.orm.query_extension.pagination`.
+     * This ensures other filters can have precedence over localization.
+     *
+     * @see https://api-platform.com/docs/core/extensions/#custom-doctrine-orm-extension
+     */
+    public static function getDefaultPriority(): int
+    {
+        return -64;
+    }
+
+    public function supportsResult(string $resourceClass, ?Operation $operation = null, array $context = []): bool
+    {
+        $reflectionClass = new \ReflectionClass($resourceClass);
+
+        return $reflectionClass->implementsInterface(LocalizedContentInterface::class);
+    }
+
+    public function getResult(
+        QueryBuilder $queryBuilder,
+        ?string $resourceClass = null,
+        ?Operation $operation = null,
+        array $context = []
+    ): ?object {
+        $query = $this->addLocalizationHints($queryBuilder, $this->getContextLanguages($context));
+
+        return $query->getOneOrNullResult();
+    }
+
+    public function applyToItem(
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $resourceClass,
+        array $identifiers,
+        ?Operation $operation = null,
+        array $context = []
+    ): void {}
+}
